8eabe13b73a9eba01d15a95de1564915d655a88a
Unknown <zahart76@gmail.com>
2018-10-17 17:10:07 +0300

    Adminnistration 1

---
+++ levels.py
@@ -0,0 +1,30 @@
+def entrance_level(window, height, width):
+    def tree(y, x):
+        for stick in range(8):
+            for i in range(5):
+                window.addstr(y-stick, x+i, "I")
+                highy = y-stick
+        for leaves_length in range(10):
+            for leaves_width in range(9):
+                window.addstr(highy-leaves_length, x-2+leaves_width, "Ж")
+        for leaves_length in range(8):
+            for leaves_width in range(2):
+                window.addstr(highy-1-leaves_length, x-4+leaves_width, "Ж")
+            for leaves_width in range(2):
+                window.addstr(highy-1-leaves_length, x+8-leaves_width, "Ж")
+        for leaves_length in range(6):
+            for leaves_width in range(2):
+                window.addstr(highy-2-leaves_length, x+10-leaves_width, "Ж")
+            for leaves_width in range(2):
+                window.addstr(highy-2-leaves_length, x-6+leaves_width, "Ж")
+
+    for y in range((height - 1)//2):
+        for x in range(width - 1):
+            window.addstr(y, x, "H")
+    for y in range(12, (height - 1)//2):
+        for x in range(86, 118):
+            window.addstr(y, x, "🧱")
+    tree(height - 9, 40)
+    tree(height - 6, width - 50)
+    tree(height - 8, width - 20)
+    tree(height - 4, 15)
---
+++ player.py
@@ -0,0 +1,39 @@
+def movement(window, height, width, obstacle, y, x, level):
+    import curses
+    key = window.getch()
+    for area in range(5):
+        for len in range(6):
+            window.addch(y + len, x + area, ' ')
+    if key == ord('w') or key == curses.KEY_UP:
+        y -= 1
+        if chr(window.inch(y, x)) in obstacle or y == 1:
+            y += 1
+    if key == ord('s') or key == curses.KEY_DOWN:
+        y += 1
+        if y == height - 7:
+            y -= 1
+        elif chr(window.inch(y + 6, x)) in obstacle:
+            y -= 1
+    if key == ord('d') or key == curses.KEY_RIGHT:
+        x += 1
+        if chr(window.inch(y, x + 4)) in obstacle or x == width - 5:
+            x -= 1
+    if key == ord('a') or key == curses.KEY_LEFT:
+        x -= 1
+        if chr(window.inch(y, x)) in obstacle or x == 1:
+            x += 1
+    if key == ord('q'):
+        return y, x, False, level
+    if chr(window.inch(y, x)) == '🧱':
+        level += 1
+        return y, x, True, level
+    return y, x, True, level
+
+
+def body(window, y, x):
+    window.addstr(y, x+1, "_П_")
+    window.addstr(y+1, x+2, "@")
+    window.addstr(y+2, x, "--|--")
+    window.addstr(y+3, x, "  |  ")
+    window.addstr(y+4, x+1, "| |")
+    window.addstr(y+5, x+1, "| |")
