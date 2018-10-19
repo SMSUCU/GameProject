2c0a26e751f3b6ebc62c768aaa91dd5b65fbfcaf
Unknown <zahart76@gmail.com>
2018-10-19 14:05:51 +0300

    Syntax

--- parni.py
+++ parni.py
@@ -1,21 +1,19 @@
-def parni ():
+def parni():
     import random
 
     a = random.randint(1, 100000)
     b = random.randint(1, 100000)
 
-    def perevirka (a)
+    def perevirka(a):
         if (a//2 == 0):
             return True
         else:
             return False
 
-    def suma(a,b):
+    def suma(a, b):
         if (a+b)//2 == 0:
             return True
         else:
             return False
 
-    return perevirka()
-    return suma()
-print(parni())
+    return perevirka, suma()
