0076fa6a5822d78e4ba449d84abe11d886a9377e
Unknown <zahart76@gmail.com>
2018-10-22 12:44:23 +0300

    commit

--- parni.py
+++ parni.py
@@ -5,15 +5,18 @@ def parni():
     b = random.randint(1, 100000)
 
     def perevirka(a):
-        if (a//2 == 0):
+        if (a % 2 == 0):
             return True
         else:
             return False
 
     def suma(a, b):
-        if (a+b)//2 == 0:
+        if (a+b) % 2 == 0:
             return True
         else:
             return False
 
-    return perevirka, suma()
+    return perevirka(a), suma(a, b)
+
+
+print(parni())
--- parni1.py
+++ parni1.py
@@ -4,15 +4,16 @@ def parni():
     a = random.randint(1, 4)
     b = random.randint(1, 4)
     print(a, b)
-    def perevirka (a):
+
+    def perevirka(a):
         if a % 2 != 0:
             return False
         else:
             return True
 
-    def suma(a,b):
-        if (a+b)%2 == 0:
+    def suma(a, b):
+        if (a+b) % 2 == 0:
             return True
         else:
             return False
-    return(perevirka(a), suma(a,b))
+    return(perevirka(a), suma(a, b))
