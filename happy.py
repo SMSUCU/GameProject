cdf6d730fcecee97900d4478b728025cfdeb782f
Unknown <zahart76@gmail.com>
2018-10-22 12:32:54 +0300

    Happy func

--- happy.py
+++ happy.py
@@ -1,21 +1,22 @@
-import random
-import buttons
-import happyfunc
-# lst = [1, 7, 10, 13, 19, 23, 28, 31, 32, 44,
-#        49, 68, 70, 79, 82, 86, 91, 94, 97, 100, 103, 109,
-#        129, 130, 133, 139, 167, 176, 188, 190, 192, 193]
-lst = happyfunc.checkhappy(201)
-lst1 = list(range(201))
-lst1.extend(lst)
-number = random.choice(lst1)
-print('Is', number, 'a happy number?')
-if number in lst:
-    value = True
-else:
-    value = False
+def happy_test:
+    import random
+    import buttons
+    import happyfunc
+    # lst = [1, 7, 10, 13, 19, 23, 28, 31, 32, 44,
+    #        49, 68, 70, 79, 82, 86, 91, 94, 97, 100, 103, 109,
+    #        129, 130, 133, 139, 167, 176, 188, 190, 192, 193]
+    lst = happyfunc.checkhappy(201)
+    lst1 = list(range(201))
+    lst1.extend(lst)
+    number = random.choice(lst1)
+    print('Is', number, 'a happy number?')
+    if number in lst:
+        value = True
+    else:
+        value = False
 
-player_value = buttons.buttons()
-if player_value == value:
-    print('Correct')
-else:
-    print('Wrong')
+    player_value = buttons.buttons()
+    if player_value == value:
+        print('Correct')
+    else:
+        print('Wrong')
