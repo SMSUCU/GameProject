d45cd33faee9691a6883c9e82a9045719ef66ec8
sofiagarkot <44227901+sofiagarkot@users.noreply.github.com>
2018-10-22 13:09:36 +0300

    Add files via upload

---
+++ test4all.py
@@ -0,0 +1,13 @@
+def test_all():
+    import random
+    import parni
+    import happy
+    import ulam
+    a = random.randint(1,100)
+    if perevirka(a) == True:
+        value = 1
+    if happy() == True:
+        value = 2
+    if ulamfunc() == True:
+        value = 3
+    return value == player_value
