c875fdd7a208ad918f47275c2cc7c003978c7d37
Unknown <zahart76@gmail.com>
2018-10-23 20:15:29 +0300

    Fix

--- SMS_happy.py
+++ SMS_happy.py
@@ -6,10 +6,6 @@ def checkhappy():
     '''
     None -> list
     This function checks if a number is a happy one or a sad and adds it to a list
-
-    >>> checkhappy(201)
-    [1, 7, 10, 13, 19, 23, 28, 31, 32, 44, 49, 68, 70, 79, 82, 86, 91, 94, 97, 100, 103, 109, 129, 130, 133, 139, 167, 176, 188, 190, 192, 193]
-
     '''
     lst = []
     for num in range(1, 101):
--- SMS_test4all.py
+++ SMS_test4all.py
@@ -9,7 +9,7 @@ def test_all(height, width, window):
     '''(int, int, window) -> bool
     This function returns result of test for all number types.
     '''
-    number = random.randint(1, 200)
+    number = random.randint(1, 100)
     value = []
     y, x = height // 2 - 4, width // 2 - 4
     window.addstr(y, x, 'What type of number is ' + str(number) + '?')
